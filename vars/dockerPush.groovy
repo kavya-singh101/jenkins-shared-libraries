734f80449a70b1f936d502ce483a01b297e89909
kavya-singh101 <130764087+kavya-singh101@users.noreply.github.com>
2025-02-20 21:34:02 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,8 @@
 def call(String Project, String ImageTag){
   withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
       sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+    sh "sudo docker image tag ${Project}:${ImageTag} ${env.dockerHubUser}/${Project}:${ImageTag}"
+    sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
   }
-  sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+  
 }
